39e134b58bde2371ec66c2c2364a47091498d053
gappeagy <appeagyeigeorgina@gmail.com>
2025-07-24 12:58:19 +0100

    render artist names to index.html

---
+++ artists.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Artists() ([]Artist, error) {
+	// get response from project API
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		log.Printf("HTTP request failed with status code %d", response.StatusCode)
+		os.Exit(1)
+	}
+
+	// convert byte stream to []byte then a string
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(string(responseData))
+
+	var artists []Artist
+	err = json.Unmarshal(responseData, &artists)
+	if err != nil {
+		log.Fatal("Error unmarshling JSON: ", err)
+	}
+
+	// for _, artist := range artists {
+	// 	fmt.Println(artist.Members)
+	// }
+
+	return artists, nil
+}
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func homeHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		// TODO: add 404 page template
+	}
+
+	template, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(writer, "Error parsing index.html template", http.StatusInternalServerError)
+		return
+	}
+
+	artistsData, err := Artists()
+	if err != nil {
+		http.Error(writer, "Trouble loading artists", http.StatusInternalServerError)
+	}
+
+	artistNames := make([]string, len(artistsData))
+
+	fmt.Println(artistsData)
+
+	for i, artist := range artistsData {
+		artistNames[i] = artist.Name
+	}
+
+	template.Execute(writer, artistNames)
+
+}
