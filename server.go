c959dfa7a9c61726fcd5ad55e28acbc5d6fcab8a
gappeagy <appeagyeigeorgina@gmail.com>
2025-07-29 14:23:40 +0100

    artist pages backend logic

--- server.go
+++ server.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 func runServer() {
 	http.HandleFunc("/browse", browseHandler)
+	http.HandleFunc("/artists/", artistHandler)
 
+	// link css
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
@@ -35,4 +38,31 @@ func browseHandler(writer http.ResponseWriter, request *http.Request) {
 
 	template.Execute(writer, artistsData)
 
+}
+
+func artistHandler(writer http.ResponseWriter, request *http.Request) {
+	name := strings.TrimPrefix(request.URL.Path, "/artists/")
+
+	template, err := template.ParseFiles("templates/artists/artist.html")
+	if err != nil {
+		http.Error(writer, "Template error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// an array of artist data for each artist struct
+	artistsData, err := Artists()
+	if err != nil {
+		http.Error(writer, "Trouble loading artists", http.StatusInternalServerError)
+		return
+	}
+
+	// find artist using name
+	for _, artist := range artistsData {
+		if artist.Name == name {
+			template.Execute(writer, artist)
+			return
+		}
+	}
+
+	http.NotFound(writer, request)
 }
